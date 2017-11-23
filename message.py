5485e559832f49718d39621f6e188dc9711854ac
brx <b0r1sp@users.noreply.github.com>
2017-11-23 20:11:20 +0100

    Update message.py

--- message.py
+++ message.py
@@ -1,5 +1,5 @@
 from skpy import Skype
-sk = Skype("boristimp@gmail.com", "skype2301")
+sk = Skype("xxxxx", "yyyy")
 ch = sk.chats.create()
 ch.sendMsg("Changed Backgroungimage on desktop!")
 print("Message send")
